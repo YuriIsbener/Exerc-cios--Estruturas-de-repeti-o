7a7c262013c057c0786a84494a719e62a0d6ed49
Yuri Isbener <yurisuhett@gmail.com>
2020-11-18 18:26:12 -0300

    Segunda atividade

--- Program.cs
+++ Program.cs
@@ -6,13 +6,27 @@ namespace Nova_pasta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite um valor ");
-                int numero = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite um valor ");
+                //int numero = int.Parse(Console.ReadLine());
 
-            while(numero > 10 || numero <= 0)
-            {   
-                Console.WriteLine("Valor invalido, tente novamente ");
-                int.Parse(Console.ReadLine());
+            //while(numero > 10 || numero <= 0)
+            //{   
+                //Console.WriteLine("Valor invalido, tente novamente ");
+                //int.Parse(Console.ReadLine());
+            //}
+
+            Console.Write("Usuário: ");
+            string usuario = Console.ReadLine();
+            Console.Write("Senha: ");
+            string senha = Console.ReadLine();
+
+            while(senha == usuario)
+            {
+                Console.WriteLine("Senha ou usuário invalidos, por favor tente novamente");
+                Console.Write("Usuário: ");
+                usuario = Console.ReadLine();
+                Console.Write("Senha: ");
+                senha = Console.ReadLine();
             }
 
         }
