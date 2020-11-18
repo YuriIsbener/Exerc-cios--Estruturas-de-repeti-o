707583de2cc86528e04b5d43c2667fc6fcee6fa6
Yuri Isbener <yurisuhett@gmail.com>
2020-11-18 18:13:39 -0300

    Primeira atividade

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nova_pasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor ");
+                int numero = int.Parse(Console.ReadLine());
+
+            while(numero > 10 || numero <= 0)
+            {   
+                Console.WriteLine("Valor invalido, tente novamente ");
+                int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
