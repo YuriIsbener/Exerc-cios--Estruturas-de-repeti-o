99be3c51fd0e915bf9f5193f62e6be2eb17a9dbc
Yuri Isbener <yurisuhett@gmail.com>
2020-11-18 21:20:55 -0300

    terceira atividade

--- Program.cs
+++ Program.cs
@@ -15,19 +15,45 @@ namespace Nova_pasta
                 //int.Parse(Console.ReadLine());
             //}
 
-            Console.Write("Usuário: ");
-            string usuario = Console.ReadLine();
-            Console.Write("Senha: ");
-            string senha = Console.ReadLine();
+            //Console.Write("Usuário: ");
+            //string usuario = Console.ReadLine();
+            //Console.Write("Senha: ");
+            //string senha = Console.ReadLine();
 
-            while(senha == usuario)
+            //while(senha == usuario)
+            //{
+                //Console.WriteLine("Senha ou usuário invalidos, por favor tente novamente");
+                //Console.Write("Usuário: ");
+                //usuario = Console.ReadLine();
+                //Console.Write("Senha: ");
+                //senha = Console.ReadLine();
+            //}
+
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            while(String.IsNullOrEmpty(nome))
             {
-                Console.WriteLine("Senha ou usuário invalidos, por favor tente novamente");
-                Console.Write("Usuário: ");
-                usuario = Console.ReadLine();
-                Console.Write("Senha: ");
-                senha = Console.ReadLine();
+             Console.Write("Nome inválido, tente novamente");
+             nome = Console.ReadLine();   
             }
+            Console.Write("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            while(idade < 0 || idade > 150)
+            {
+                Console.Write("Idade inválida, tente novamente");
+                idade = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Digite seu salário: ");
+            float salario = float.Parse(Console.ReadLine());
+            while(salario < 0)
+            {
+                Console.Write("Salário inválida, tente novamente");
+                salario = float.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Qual seu estado civil?");
+            Console.WriteLine("'s'(solteiro(a), 'c'(casado(a), 'v'(viuvo(a), 'd'(divorciado(a)");
+            char estado = char.Parse(Console.ReadLine());
+            
 
         }
     }
